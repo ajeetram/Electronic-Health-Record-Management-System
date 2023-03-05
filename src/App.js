34b42f30797439b4953e59a47d9518ebd1e972c7
ajeetram <86039147+ajeetram@users.noreply.github.com>
2023-03-06 03:48:24 +0530

    Error Resolved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React,{useEffect,useContext} from 'react';
 import './App.css';
+import {Navbar, AllUser, Chat} from './Components/Index'
+//import {AllUser} from './Components/Index'
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
+ 
+
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Navbar></Navbar>
+      {/* <AllUser /> */}
+      <Routes>
+      <Route path="/alluser" element={<AllUser />} />
+      <Route path="/" element={<Chat />} />
+      <Route path="/:queryParam1/:queryParam2" element={<Chat />} />
+      
+              {/* <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} /> */}
+      </Routes>
+    </>
   );
 }
 
